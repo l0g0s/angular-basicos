047c9099ee46f006c1e02e766dc59098418e6d70
alvaro.herrera <alvaro.herrera@euigs.com>
2021-03-05 17:30:56 +0100

    desde trabajo1

---
+++ src/app/dbz/main-page/main-page.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interface';
+
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main-page.component.html'
+})
+export class MainPageComponent {
+
+  nuevo: Personaje = {
+    nombre:'Maestro Roshi',
+    poder:1000
+  }
+  
+  personajes: Personaje[] = [
+    {
+      nombre: "Goku",
+      poder: 150000
+    },
+    {
+      nombre: "Vegeta",
+      poder: 7500
+    }
+  ]
+
+}
